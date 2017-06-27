384021b426c2ee3202da7bda61574e99a98a4f8f
ilnuribat <ilnuribat@gmail.com>
2017-06-27 11:27:34 +0300

    changed port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('json spaces', 4);
-const port = 8085;
+const port = 8080;
 
 app.use('/books', require('./books'));
 
